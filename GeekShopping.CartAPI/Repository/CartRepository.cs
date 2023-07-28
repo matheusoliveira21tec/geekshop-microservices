e3e1c6b2e5ac5081fa510547f84688dbea066f35
Matheus Oliveira <mhoa.2000@gmail.com>
2023-07-27 23:45:13 -0300

    consumindo microservico de cupom + tela de checkout

--- GeekShopping.CartAPI/Repository/CartRepository.cs
+++ GeekShopping.CartAPI/Repository/CartRepository.cs
@@ -19,7 +19,30 @@ public class CartRepository : ICartRepository
 
     public async Task<bool> ApplyCoupon(string userId, string couponCode)
     {
-        throw new NotImplementedException();
+        var header = await _context.CartHeaders
+                    .FirstOrDefaultAsync(c => c.UserId == userId);
+        if (header != null)
+        {
+            header.CouponCode = couponCode;
+            _context.CartHeaders.Update(header);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        return false;
+    }
+
+    public async Task<bool> RemoveCoupon(string userId)
+    {
+        var header = await _context.CartHeaders
+                    .FirstOrDefaultAsync(c => c.UserId == userId);
+        if (header != null)
+        {
+            header.CouponCode = "";
+            _context.CartHeaders.Update(header);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        return false;
     }
 
     public async Task<bool> ClearCart(string userId)
@@ -49,11 +72,6 @@ public class CartRepository : ICartRepository
             .Where(c => c.CartHeaderId == cart.CartHeader.Id)
                 .Include(c => c.Product);
         return _mapper.Map<CartVO>(cart);
-    }
-
-    public async Task<bool> RemoveCoupon(string userId)
-    {
-        throw new NotImplementedException();
     }
 
     public async Task<bool> RemoveFromCart(long cartDetailsId)
